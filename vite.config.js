288eff7164a58309a7eb2c13eaba5b1a583eee4f
jason101o <jason101o@gmail.com>
2021-11-03 11:05:10 +0800

    add deploy.sh

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/JsUnderGround/dist/',
   plugins: [vue()],
   alias: {
     "@": path.resolve(__dirname, "src"),
